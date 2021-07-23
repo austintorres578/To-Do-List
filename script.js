195d75787429f60aca04c216c90f9edea6c272e7
Austin Torres <78386688+austintorres578@users.noreply.github.com>
2021-07-23 14:29:09 -0400

    Daily Inputs/notes are now functional
    Added functionality to daily inputs/notes. can now write notes and delete them as well as with the daily shedule.

--- script.js
+++ script.js
@@ -198,6 +198,8 @@ function resetLine7(){
 
 //5 THINGS GRATEFUL FOR
 
+//VARIABLE AREAS
+
 let gratefulEmptyInput1 = document.getElementById("gratefulEmptyInput1");
 let gratefulWriteButton1 = document.getElementById("grateful-write-button1");
 let gratefulCheckButton1 = document.getElementById("grateful-check-button1");
@@ -218,7 +220,7 @@ let gratefulEmptyInput5 = document.getElementById("gratefulEmptyInput5");
 let gratefulWriteButton5 = document.getElementById("grateful-write-button5");
 let gratefulCheckButton5 = document.getElementById("grateful-check-button5");
 
-
+//EVENT LISTENERS
 
 gratefulEmptyInput1.addEventListener("click",gratefulFillLine1);
 gratefulWriteButton1.addEventListener("click",gratefulWriteLine1);
@@ -240,6 +242,7 @@ gratefulEmptyInput5.addEventListener("click",gratefulFillLine5);
 gratefulWriteButton5.addEventListener("click",gratefulWriteLine5);
 gratefulCheckButton5.addEventListener("click",gratefulResetLine5)
 
+//FUNCTIONS
 
 function gratefulFillLine1(){
    gratefulEmptyInput1.style.width = "90%";
@@ -337,3 +340,238 @@ function gratefulResetLine5(){
 
 // GRATEFUL SECTION END
 
+//NOTES SECTION
+
+let noteWriteBtn =document.getElementById("notes-write-btn");
+let noteTrashBtn =document.getElementById("notes-trash-btn");
+let noteBox = document.getElementById("notes-box");
+let noteBoxText = document.getElementById("note-box-text");
+let filledNoteBox =document.getElementById("filled-notes-box");
+let filledNoteBoxText = document.getElementById("filled-note-box-text");
+
+
+noteWriteBtn.addEventListener("click", writeNote);
+noteTrashBtn.addEventListener("click", trashNote);
+
+function writeNote(){
+   filledNoteBoxText.value=noteBoxText.value;
+   noteBoxText.value="";
+   noteBox.style.display = "none";
+   filledNoteBox.style.display = "block";
+};
+
+function trashNote(){
+   filledNoteBoxText.value="";
+   filledNoteBox.style.display ="none";
+   noteBox.style.display ="block";
+}
+
+//DAILY SECTION
+let monEmptyCon = document.getElementById("mon-empty-day-input");
+let monEmptyInput = document.getElementById("mon-empty-input");
+let monWriteBtn = document.getElementById("mon-write-btn");
+let monFilledDayCon =document.getElementById("mon-filled-day");
+let monFilledInput = document.getElementById("mon-filled-text");
+let monCheckBtn = document.getElementById('mon-check-btn');
+
+monEmptyInput.addEventListener("click",toggleMonWrite);
+monWriteBtn.addEventListener("click",toggleMonFilledDay)
+monCheckBtn.addEventListener("click",toggleMonEmptyInput);
+
+function toggleMonWrite(){
+   monEmptyInput.style.width ="90%";
+   monWriteBtn.style.display="block";
+};
+function toggleMonFilledDay(){
+   monEmptyInput.style.width="100%";
+   monWriteBtn.style.display="none";
+   monEmptyCon.style.display="none";
+   monFilledDayCon.style.display="flex";
+   monFilledInput.value=monEmptyInput.value;
+   monEmptyInput.value="";
+};
+function toggleMonEmptyInput(){
+   monEmptyCon.style.display="flex";
+   monFilledInput.value="";
+   monFilledDayCon.style.display="none";
+};
+
+
+let tuesEmptyCon = document.getElementById("tues-empty-day-input");
+let tuesEmptyInput = document.getElementById("tues-empty-input");
+let tuesWriteBtn = document.getElementById("tues-write-btn");
+let tuesFilledDayCon =document.getElementById("tues-filled-day");
+let tuesFilledInput = document.getElementById("tues-filled-text");
+let tuesCheckBtn = document.getElementById('tues-check-btn');
+
+tuesEmptyInput.addEventListener("click",toggleTuesWrite);
+tuesWriteBtn.addEventListener("click",toggleTuesFilledDay)
+tuesCheckBtn.addEventListener("click",toggleTuesEmptyInput);
+
+function toggleTuesWrite(){
+   tuesEmptyInput.style.width ="90%";
+   tuesWriteBtn.style.display="block";
+};
+function toggleTuesFilledDay(){
+   tuesEmptyInput.style.width="100%";
+   tuesWriteBtn.style.display="none";
+   tuesEmptyCon.style.display="none";
+   tuesFilledDayCon.style.display="flex";
+   tuesFilledInput.value=tuesEmptyInput.value;
+   tuesEmptyInput.value="";
+};
+function toggleTuesEmptyInput(){
+   tuesEmptyCon.style.display="flex";
+   tuesFilledInput.value="";
+   tuesFilledDayCon.style.display="none";
+};
+
+
+let wedEmptyCon = document.getElementById("wed-empty-day-input");
+let wedEmptyInput = document.getElementById("wed-empty-input");
+let wedWriteBtn = document.getElementById("wed-write-btn");
+let wedFilledDayCon =document.getElementById("wed-filled-day");
+let wedFilledInput = document.getElementById("wed-filled-text");
+let wedCheckBtn = document.getElementById('wed-check-btn');
+
+wedEmptyInput.addEventListener("click",toggleWedWrite);
+wedWriteBtn.addEventListener("click",toggleWedFilledDay)
+wedCheckBtn.addEventListener("click",toggleWedEmptyInput);
+
+function toggleWedWrite(){
+   wedEmptyInput.style.width ="90%";
+   wedWriteBtn.style.display="block";
+};
+function toggleWedFilledDay(){
+   wedEmptyInput.style.width="100%";
+   wedWriteBtn.style.display="none";
+   wedEmptyCon.style.display="none";
+   wedFilledDayCon.style.display="flex";
+   wedFilledInput.value=wedEmptyInput.value;
+   wedEmptyInput.value="";
+};
+function toggleWedEmptyInput(){
+   wedEmptyCon.style.display="flex";
+   wedFilledInput.value="";
+   wedFilledDayCon.style.display="none";
+};
+
+
+let thursEmptyCon = document.getElementById("thurs-empty-day-input");
+let thursEmptyInput = document.getElementById("thurs-empty-input");
+let thursWriteBtn = document.getElementById("thurs-write-btn");
+let thursFilledDayCon =document.getElementById("thurs-filled-day");
+let thursFilledInput = document.getElementById("thurs-filled-text");
+let thursCheckBtn = document.getElementById('thurs-check-btn');
+
+thursEmptyInput.addEventListener("click",toggleThursWrite);
+thursWriteBtn.addEventListener("click",toggleThursFilledDay)
+thursCheckBtn.addEventListener("click",toggleThursEmptyInput);
+
+function toggleThursWrite(){
+   thursEmptyInput.style.width ="90%";
+   thursWriteBtn.style.display="block";
+};
+function toggleThursFilledDay(){
+   thursEmptyInput.style.width="100%";
+   thursWriteBtn.style.display="none";
+   thursEmptyCon.style.display="none";
+   thursFilledDayCon.style.display="flex";
+   thursFilledInput.value=thursEmptyInput.value;
+   thursEmptyInput.value="";
+};
+function toggleThursEmptyInput(){
+   thursEmptyCon.style.display="flex";
+   thursFilledInput.value="";
+   thursFilledDayCon.style.display="none";
+};
+
+
+let friEmptyCon = document.getElementById("fri-empty-day-input");
+let friEmptyInput = document.getElementById("fri-empty-input");
+let friWriteBtn = document.getElementById("fri-write-btn");
+let friFilledDayCon =document.getElementById("fri-filled-day");
+let friFilledInput = document.getElementById("fri-filled-text");
+let friCheckBtn = document.getElementById('fri-check-btn');
+
+friEmptyInput.addEventListener("click",toggleFriWrite);
+friWriteBtn.addEventListener("click",toggleFriFilledDay)
+friCheckBtn.addEventListener("click",toggleFriEmptyInput);
+
+function toggleFriWrite(){
+   friEmptyInput.style.width ="90%";
+   friWriteBtn.style.display="block";
+};
+function toggleFriFilledDay(){
+   friEmptyInput.style.width="100%";
+   friWriteBtn.style.display="none";
+   friEmptyCon.style.display="none";
+   friFilledDayCon.style.display="flex";
+   friFilledInput.value=friEmptyInput.value;
+   friEmptyInput.value="";
+};
+function toggleFriEmptyInput(){
+   friEmptyCon.style.display="flex";
+   friFilledInput.value="";
+   friFilledDayCon.style.display="none";
+};
+
+
+let satEmptyCon = document.getElementById("sat-empty-day-input");
+let satEmptyInput = document.getElementById("sat-empty-input");
+let satWriteBtn = document.getElementById("sat-write-btn");
+let satFilledDayCon =document.getElementById("sat-filled-day");
+let satFilledInput = document.getElementById("sat-filled-text");
+let satCheckBtn = document.getElementById('sat-check-btn');
+
+satEmptyInput.addEventListener("click",toggleSatWrite);
+satWriteBtn.addEventListener("click",toggleSatFilledDay);
+satCheckBtn.addEventListener("click",toggleSatEmptyInput);
+
+function toggleSatWrite(){
+   satEmptyInput.style.width ="90%";
+   satWriteBtn.style.display="block";
+};
+function toggleSatFilledDay(){
+   satEmptyInput.style.width="100%";
+   satWriteBtn.style.display="none";
+   satEmptyCon.style.display="none";
+   satFilledDayCon.style.display="flex";
+   satFilledInput.value=satEmptyInput.value;
+   satEmptyInput.value="";
+};
+function toggleSatEmptyInput(){
+   satEmptyCon.style.display="flex";
+   satFilledInput.value="";
+   satFilledDayCon.style.display="none";
+};
+
+
+let sunEmptyCon = document.getElementById("sun-empty-day-input");
+let sunEmptyInput = document.getElementById("sun-empty-input");
+let sunWriteBtn = document.getElementById("sun-write-btn");
+let sunFilledDayCon =document.getElementById("sun-filled-day");
+let sunFilledInput = document.getElementById("sun-filled-text");
+let sunCheckBtn = document.getElementById('sun-check-btn');
+
+sunEmptyInput.addEventListener("click",toggleSunWrite);
+sunWriteBtn.addEventListener("click",toggleSunFilledDay);
+sunCheckBtn.addEventListener("click",toggleSunEmptyInput);
+
+function toggleSunWrite(){
+   sunEmptyInput.style.width ="90%";
+   sunWriteBtn.style.display="block";
+};
+function toggleSunFilledDay(){
+   sunEmptyInput.style.width="100%";
+   sunWriteBtn.style.display="none";
+   sunEmptyCon.style.display="none";
+   sunFilledDayCon.style.display="flex";
+   sunFilledInput.value=sunEmptyInput.value;
+   sunEmptyInput.value="";
+};
+function toggleSunEmptyInput(){
+   sunEmptyCon.style.display="flex";
+   sunFilledInput.value="";
+   sunFilledDayCon.style.display="none";
+};
